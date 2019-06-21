47632ad738bd7b4e6c3677b11ee731c03efae946
guangzhuo <guangzhuo.zheng@socialcredits.cn>
2019-06-21 09:41:35 +0800

    环境变量 配置

---
+++ src/config/dev.env.js
@@ -0,0 +1,6 @@
+const hosturl= 'http://127.0.0.1:10021';
+// 本地环境
+module.exports = {
+    NODE_ENV: '"development"',
+    hosturl:hosturl
+}
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,46 @@
-module.exports = {
-    // 修改的配置
-    // 将baseUrl: '/api',改为baseUrl: '/',
-    baseUrl: '/',
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://www.example.org',
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {
-                  '^/api': ''
-                }
-            }
-        }
-    }
+// var path = require('path');
+
+const devProxy = ['/pc','/weixin','android'];  // 代理
+var proEnv = require('./src/config/pro.env');  // 生产环境
+var uatEnv = require('./src/config/test.env');  // 测试环境
+var devEnv = require('./src/config/dev.env');  // 本地环境
+const env = process.env.NODE_ENV;
+
+let target = '';
+// 默认是本地环境
+if(env === 'production'){  // 生产环境
+    target = proEnv.hosturl;
+}else if(env === 'test'){ // 测试环境
+    target = uatEnv.hosturl;
+}else{  // 本地环境
+    target = devEnv.hosturl;
 }
+console.log(target)
+console.log(env)
+// 生成代理配置对象
+let proxyObj = {};
+devProxy.forEach((value) => {
+    console.log(value)
+    proxyObj[value] = {
+        target: target,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^${value}`]: value
+        }
+    };
+});
+
+module.exports = {
+    publicPath: '/',
+    outputDir: 'dist',
+    devServer: {
+        // open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        disableHostCheck: true,
+        // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
+        proxy: proxyObj // string | Object
+    }
+};
